144f52988b2aeaf63a37763d38f6606475195a88
Victor T. <pro_victor@ymail.com>
2015-08-07 14:11:14 -0700

    Update SkinTools.php

--- SkinTools/src/skintools/SkinTools.php
+++ SkinTools/src/skintools/SkinTools.php
@@ -3,9 +3,12 @@
 namespace skintools;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use skintools\command\SkinToolsCommand;
 use skintools\event\SkinToolsListener;
 
 class SkinTools extends PluginBase{
+    public $skintools;
     public function onEnable(){
 	$this->registerAll();
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
@@ -14,6 +17,17 @@ class SkinTools extends PluginBase{
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
     private function registerAll(){
+    	$this->skintools = array();
+    	$this->getServer()->getCommandMap()->register("skintools", new SkinToolsCommand($this), $this);
     	$this->getServer()->getPluginManager()->registerEvents(new SkinToolsListener($this), $this);
+    }
+    public function setStolenSkin(Player $player1, Player $player2){
+    	$player1->setSkin($player2->getSkinData());
+    }
+    public function setTouchMode(Player $player, $touchMode = true){
+    	if(is_bool($skinOnTouch)) $this->skintools["touchMode"][strtolower($player->getName())] = $touchMode;
+    }
+    public function hasTouchMode(Player $player){
+    	return $this->skintools["touchMode"][strtolower($player->getName())] === true;
     }
 }
